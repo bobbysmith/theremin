8bc18fbbb8fca7e3ea5376f21ff3c3f26a6155c8
Bobby Smith <bobbysmith24@gmail.com>
2016-09-17 18:24:07 -0500

    updated code form codepen

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var context = new AudioContext();
+oscillator = context.createOscillator();
+gainNode = context.createGain();
+oscillator.connect(gainNode);
+gainNode.connect(context.destination);
+// oscillator.start();
+
 var container = document.querySelector('.container');
 var stop = document.querySelector('.stop');
 var start = document.querySelector('.start');
@@ -6,39 +13,33 @@ var vol = document.querySelector('.volume');
 var height = container.scrollHeight;
 var width = container.scrollWidth;
 var volumeStep = height / 100;
-var freqStep = width / 2000;
+var freqStep = width / 4980;
 var x, y;
 
-var context = new AudioContext();
-// oscillator.start();
-
 container.addEventListener('resize', function() {
-  height = container.scrollHeight;
+	height = container.scrollHeight;
   width = container.scrollWidth;
-  volumeStep = height / 100;
-  freqStep = width / 4980;
+	volumeStep = height / 100;
+	freqStep = width / 4980;
 });
 
-container.addEventListener('mousedown', function(e) {
-  x = e.clientX;
-  y = e.clientY;
-  if (x > 0) {
-    oscillator.frequency.value = (x / freqStep).toFixed(0);
-    freq.innerHTML = (x / freqStep).toFixed(0) + 'Hz';
-  }
-  gainNode.gain.value = Math.round(y / volumeStep) / 100;
-  vol.innerHTML = Math.round(y / volumeStep) + '%';
+container.addEventListener('mousemove', function(e) {
+	x = e.clientX;
+	y = e.clientY;
+	if (x > 0) {
+		oscillator.frequency.value = (x / freqStep).toFixed(0);	
+		freq.innerHTML = (x / freqStep).toFixed(0) + 'Hz';
+	}
+	gainNode.gain.value = Math.round(y / volumeStep) / 100;
+	
+	vol.innerHTML = Math.round(y / volumeStep) + '%';
 });
 
 start.addEventListener('click', function() {
-  oscillator = context.createOscillator();
-  gainNode = context.createGain();
-  oscillator.connect(gainNode);
-  gainNode.connect(context.destination);
-  oscillator.start();
+	oscillator.start();
 });
 
 
 stop.addEventListener('click', function() {
-  oscillator.stop();
+	oscillator.stop();
 });
